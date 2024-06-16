1f99d24f3f05f26df6fb56ab6a01c484e73dba4c
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-16 23:20:43 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,23 @@
+from database import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Boolean
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    contacts = relationship("Contact", back_populates="owner")
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    phone_number = Column(String, index=True)
+    birthday = Column(Date)
+    additional_info = Column(String, nullable=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="contacts")
